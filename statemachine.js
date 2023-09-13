c5b6a2cdbbe77a319edbc0272cc9e80e9f91e706
bem130 <070617haruki@gmail.com>
2023-09-13 17:30:21 +0900

    add indent

--- statemachine.js
+++ statemachine.js
@@ -9,23 +9,37 @@ function main(filename) {
     let fdata = fRead(filename)
     //console.log(fdata)
     let ret = []
+    let state = ""
+    ret.push(`let s = state.split(".")`)
     ret.push("if (false) {}")
+    const retpush = (transion,sw) => {
+        if (transion[1].endsWith("Error")) {
+            ret.push(`  ${sw} (s[1]=="${transion[0].split(".")[1]}"${procCond(transion[2])}) { throw this.tokenizeerror(\`${transion[0]} => ${transion[1]}; ${transion[2]}\`,i) }`);
+            return
+        }
+        ret.push(`  ${sw} (s[1]=="${transion[0].split(".")[1]}"${procCond(transion[2])}) { state="${transion[1]}" }`)
+    }
     for (let line of fdata.split("\n")) {
 
-        let transion = line.replace(/\s/g,"").match(/^.*?(?=-)|(?<=>).*?(?=:)|(?<=:).+/g)
-        if (transion!=null&&transion.length==3) {
-            if (transion[0]==transion[1]) {
-            }
-            else if (!transion[1].endsWith("Error")) {
-                ret.push(`else if (state=="${transion[0]}"${procCond(transion[2])}) { state="${transion[1]}" }`)
+        let transion = line.replace(/\s/g, "").match(/^.*?(?=--)|(?<=>).*?(?=:)|(?<=>.*?:).+/g)
+        if (transion != null && transion.length == 3) {
+            if (transion[0] == transion[1]) { }
+            else if (state == transion[0].split(".")[0]) {
+                retpush(transion,"else if")
             }
             else {
-                ret.push(`else if (state=="${transion[0]}"${procCond(transion[2])}) { throw this.tokenizeerror(\`${transion[0]} => ${transion[1]}; ${transion[2]}\`,i) }`)
+                state = transion[0].split(".")[0]
+                ret.push(`}\nif (s[0]=="${transion[0].split(".")[0]}") {`)
+                retpush(transion,"if")
             }
+
         }
         // ["gVarDef.Colon1","gVarDef.Blank1","space"]
     }
-    //console.table(ret)
+    ret[2] = ret[2].replace("}", "")
+    ret.push("}")
+
+//console.table(ret)
     console.log("")
     console.log(ret.join("\n"))
     console.log("")
@@ -42,7 +56,7 @@ function procCond(cond) {
     let ret = ""
     for (let c of cond.split("&")) {
         let r = "";
-        if (c[0]=="!") {
+        if (c[0] == "!") {
             r = "!"
             c = c.slice(1)
         }
@@ -68,7 +82,7 @@ function procCond(cond) {
                 ret += `&&(${r}(tokenarr[tokenarr.length-1].val=="global"))`;
                 break;
             default:
-               // throw "errorrrrrrrr"
+                // throw "errorrrrrrrr"
                 break;
         }
     }
